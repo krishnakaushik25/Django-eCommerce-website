a044d481497e4ae506447b5a2772c1144ccbadf6
krishna11 <krishna11@github.com>
2021-07-16 17:10:30 +0530

    Mails and password functionality

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -134,3 +134,16 @@ STATICFILES_DIRS=[
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# SMTP configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'kaushikkrishna922@gmail.com'
+EMAIL_HOST_PASSWORD = '81R9Tje3hhiN'
+EMAIL_PORT = 587
