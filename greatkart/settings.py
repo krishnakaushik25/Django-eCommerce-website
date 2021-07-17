cef4594b710a2122f76ab4f59d21fd4a6b45a462
krishna11 <krishna11@github.com>
2021-07-17 16:25:40 +0530

    pushing to newbranch

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) # True
 
-ALLOWED_HOSTS = ['kartkrishna-env.eba-e8yajuzq.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
